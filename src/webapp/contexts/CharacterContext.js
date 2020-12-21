b58ddfd694b6038fe9ec3634da5074e6820f0541
Alex Ray <user.name>
2020-12-21 16:52:45 -0700

    added .env variables

--- src/webapp/contexts/CharacterContext.js
+++ src/webapp/contexts/CharacterContext.js
@@ -3,24 +3,8 @@ import characterReducer from '../reducers/characterReducer'
 
 export const CharacterContext = createContext();
 
-// export const useCharacterContext = () => useContext(CharacterContext)
-
 const CharacterContextProvider = (props) => {
   const [character, dispatch] = useReducer(characterReducer, null)
-    // try {
-    //   const localData = localStorage.getItem('char_info')
-    //   return localData ? JSON.parse(localData) : {}
-    // }
-    // catch (err) {
-    //   console.log(`Error in CharContext Provider: ${err}`)
-    //   return {}
-    // }
-    //})
-
-  // useEffect(() => {
-  //   console.log('SETTING CHARACTER')
-  //   localStorage.setItem('char_info', JSON.stringify(character))
-  // }, [])
 
   return (
     <CharacterContext.Provider value={{character, dispatch}}>
